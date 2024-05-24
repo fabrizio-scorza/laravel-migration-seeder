11d1e9a1d458daee558a2eb3a4c4a3593847cd88
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-24 16:18:08 +0200

    FEAT - controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    //funzione per la vista home
+    public function home()
+    {
+        // tramite il model prendo i dati della tabella train e li metto nella variabile trains
+        $trains = Train::all();
 
+        // ritorno la vista e i dati
+        return view('home', compact('trains'));
+    }
 }
